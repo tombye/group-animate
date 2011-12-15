d191586fefae3dcbfd6e0351be67337859077815
unknown <tom.byers@.readingroom.local>
2011-12-15 17:32:40 +0000

    making paths as vectors to allow for multiple rings

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@
 			// set a step callback to beacon a custom event
 			opts.step = function (now, fx) {
 				$(document).trigger(evtName, {
-						'now' : now,
 						'fx' : fx
 					}
 				);
@@ -25,8 +24,10 @@
 
 (function () {
 	var a,
-	    b = 100,
+	    b = 50,
 	    elms = [],
+		rings = [],
+		vectors = [],
 	    getPosition,
 	    angle,
 	    range,
@@ -35,7 +36,16 @@
 		getVector,
 		getPositionFromVector;
 
-	getVector = function (angle, radius) {
+	/**
+	 * Private function to get the points of a vector from its angle and length
+	 *
+	 * @function
+	 * @private
+	 * @param {Float} angle The angle of the vector
+	 * @param {Float} length The length of the vector
+	 * @returns {Object} The x,y co-ordinates of the vector
+	*/
+	getVector = function (angle, length) {
 		var circ,
 		    percent,
 		    dist,
@@ -43,13 +53,13 @@
 		    opp,
 		    adj;
 
-		circ = 2 * Math.PI * radius;
+		circ = 2 * Math.PI * length;
                 percent = angle / 360;
                 dist = circ * percent;
-                radians = dist / radius;
+                radians = dist / length;
 
-                opp = Math.sin(radians) * radius;
-                adj = Math.cos(radians) * radius;
+                opp = Math.sin(radians) * length;
+                adj = Math.cos(radians) * length;
 
 	        return {
 				'x' : opp,
@@ -57,30 +67,86 @@
 		};
 	};
 	
-	getPositionFromVector = function () {
+	/**
+	 * Private function to move a point along a vector
+	 *
+	 * @function
+	 * @private
+	 * @param {Object} point The x,y of the point to translate
+	 * @param {Float} multiplier The value to translate the vector by
+	 * @returns {Object} The new x,y co-ordinates of the point
+	*/
+	translateByVector = function (point, multiplier) {
+		point.x = point.x * multiplier;
+		point.y = point.y * multiplier;
 		
+		return point;
 	};
+	
+	/**
+	 * Private function to make a function to run every frame of animating an expanding ring of elements
+	 *
+	 * @function
+	 * @private
+	 * @param {Float} radius The final size to animate the ring to (as a percent of the largest size)
+	 * @param {Array} elms Array of elements to animate as part of the ring
+	 * @returns {Function} The function to run every frame
+	*/
+	generateFrameForRing = function (radiusScale, elms) {
+		var finalPositions = [],
+			a = vectors.length;
+		
+		while (a--) {
+			finalPositions[a] = translateByVector(vectors[a], radiusScale)
+		}
+		
+		return function (e, info) {
+			var top,
+				left,
+				a = elms.length,
+				pos = info.fx.pos;
+				
+			while(a--) {
+				top = (finalPositions[a].y * pos);
+				left = (finalPositions[a].x * pos);
+				
+				elms[a].$elm.css({
+					'top' : top + 'px',
+					'left' : left + 'px'
+				})
+			}
+		}
+	};
+
+	rings.push([]);
 
 	// let's make some coloured elements
 	for (a = 0; a < b; a++) {
-		elms.push({ '$elm' : $('<div class="node"></div>')});
-		elms[a].$elm.css('background-color', 'rgb(' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ')');
-		$('#canvas').append(elms[a].$elm);
+		rings[0].push({ '$elm' : $('<div class="node"></div>')});
+		rings[0][a].$elm.css('background-color', 'rgb(' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ')');
+		$('#canvas').append(rings[0][a].$elm);
+	}
+	
+	rings.push([]);
+	
+	// let's make some coloured elements
+	for (a = 0; a < b; a++) {
+		rings[1].push({ '$elm' : $('<div class="node"></div>')});
+		rings[1][a].$elm.css('background-color', 'rgb(' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ', ' + (Math.floor(Math.random() * 256)) + ')');
+		$('#canvas').append(rings[1][a].$elm);
 	}
 	
 	// set the range of degrees our elements will spread out to fill 
 	range = 90;
 	
-	while (a) {
-		a--;
+	for (a = 0; a < b; a++) {
 		// angle is bound to each element by it's index
 		angle = ((range / (b - 1)) * a);
-		elms[a].vector = getVector(angle, dist);
+		vectors.push(getVector(angle, dist));
 	}
 	
-	$(document).bind('frame', function (e, info) {
+	$(document).bind('frame', generateFrameForRing(1, rings[0]));
+	$(document).bind('frame', generateFrameForRing(0.8, rings[1]));
 
-	});
-
-	$.groupAnimate('frame', {'duration' : 1000})
+	$.groupAnimate('frame', {'duration' : 1000});
 }());
